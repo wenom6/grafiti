d4d7ee7021e31c84292f5fd4eeff8ebfbd7a356e
wenom6 <kyshnihivahn@gmail.com>
2025-10-17 20:03:52 +0300

    1

---
+++ grafiti/src/components/FooterComponent.vue
@@ -0,0 +1,55 @@
+<template>
+<div>
+<p>Email: support@shop.com</p>
+</div>
+</template>
+
+
+  <script>
+  export default {
+    name: "FooterComponent",
+  };
+  </script>
+  
+  <style scoped>
+  .footer {
+    background-color:rgb(255, 255, 255);
+    margin-top: 40px;
+    color: #333;
+    text-align: center;
+    border-top: 1px solid #ddd;
+  }
+  
+  .footer-content {
+    display: flex;
+    justify-content: center;
+    gap: 80px;
+    padding: 0 10%;
+  }
+  
+ul {
+    list-style: none;
+    padding: 0;
+    display:flex;
+    gap: 80px;
+  }
+  
+li {
+    margin-bottom: 5px;
+  }
+  
+a {
+    text-decoration: none;
+    color: black;
+  }
+  
+a:hover {
+    text-decoration: underline;
+  }
+ 
+  .footer-bottom {
+    margin-top: 20px;
+    font-size: 14px;
+    color: #666;
+  }
+  </style>
---
+++ grafiti/src/components/HelloWorld.vue
@@ -0,0 +1,85 @@
+<template>
+  <div >
+    <div class="main-content-wrapper"> <h1>Графіті</h1>
+    </div>
+    <p class="p">Культура графіті — це вуличне мистецтво, що виникло в кінці 1960-х років у Нью-Йорку як спосіб самовираження, що розвинувся від простих написання псевдонімів ("тегів") до складних, стилізованих зображень. 
+      </p>
+      <div><h3>Графіті не пішло від Фрески!</h3>
+      <img alt="fresca" src="https://decorcdn.com/img/catalog-240/freska-2025.jpg">
+      <img alt="graffiti" src="https://tvoemisto.tv/media/gallery/full/c/o/collage2_e8754.jpg">
+      </div>
+      <p> Фрески: Це стародавня техніка живопису на стінах, яка існувала тисячі років.</p>
+
+      <p> 
+Графіті: Термін "графіті" походить від італійського "graffiti", що означає "надряпані". Це стосується найдавніших написів, зроблених гострими предметами. Сучасне вуличне графіті, як субкультура, з'явилося у 20-му столітті. </p>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+img {
+  height: 200px;
+
+}
+
+p{
+
+  font-size: 25px;
+  
+  color:rgb(255, 255, 255); /* основний колір */
+  text-transform: uppercase;
+  text-shadow:
+    -2px -2px 0 #000,
+     2px -2px 0 #000,
+    -2px  2px 0 #000,
+     2px  2px 0 #000,
+     0  0 10px #f71909,
+     0  0 20px #ff9100;
+
+}
+
+
+.p{
+  font-family: 'Comic Sans MS', 'Impact', cursive; /* або підключи свій шрифт */
+  font-size: 30px;
+  font-weight: 900px;
+  color: #03ffc0; /* основний колір */
+  text-transform: uppercase;
+  text-shadow:
+    -2px -2px 0 #000,
+     2px -2px 0 #000,
+    -2px  2px 0 #000,
+     2px  2px 0 #000,
+     0  0 10px #f71909,
+     0  0 20px #ff9100;
+  letter-spacing: 3px;
+  transition: transform 0.3s ease, text-shadow 0.3s ease;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+
+
+</style>
